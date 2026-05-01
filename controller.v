9a5571c43fc00f88b093dde5fbb05531e6d12e5c
emreokurlar <emreokurlar.eo@gmail.com>
2026-05-01 23:17:25 +0300

    Refactor controller module to add bl_en and bx_en outputs

--- controller.v
+++ controller.v
@@ -1,24 +1,21 @@
 module controller (
     input  wire        clk,
     input  wire        reset,
-    // Instruction fields
     input  wire [1:0]  op,
     input  wire [5:0]  funct,
     input  wire [3:0]  rd,
     input  wire [3:0]  cond,
-    // Live ALU flags from datapath
     input  wire [3:0]  aluflags,
-    // Gated control outputs
     output wire        pcsrc,
     output wire        regwrite,
     output wire        memwrite,
-    // Un-gated datapath controls
     output wire        memtoreg,
     output wire        alusrc,
     output wire [1:0]  immsrc,
     output wire [1:0]  regsrc,
     output wire [2:0]  alucontrol,
-    output wire        link
+    output wire        bl_en,
+    output wire        bx_en
 );
     wire [1:0] flagw;
     wire       pcs, regw, memw;
@@ -29,7 +26,7 @@ module controller (
         .regw(regw),       .memw(memw),
         .memtoreg(memtoreg), .alusrc(alusrc),
         .immsrc(immsrc),   .regsrc(regsrc),
-        .alucontrol(alucontrol), .link(link)
+        .alucontrol(alucontrol), .bl_en(bl_en), .bx_en(bx_en)
     );
 
     condlogic cl (
